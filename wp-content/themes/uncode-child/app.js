d392dfb966a656e5f1de0f1b04d7592059601d83
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-02-21 12:14:21 -0600

    Truncate titles when on mobile to avoid overlapping

--- wp-content/themes/uncode-child/app.js
+++ wp-content/themes/uncode-child/app.js
@@ -2,5 +2,29 @@
   $( document ).ready(function() {
     $('.team-members-section #board').remove();
     $('.team-members-section div[data-id="staff"]').find('.t-entry-visual').remove();
+
+    if ($(window).width() <=800 ) {
+      truncateAllBlogTitleInMobile();
+      $( document ).ajaxComplete(truncateAllBlogTitleInMobile);
+    }    
   });
+
+  function truncateAllBlogTitleInMobile() {
+    var $postEntryTitles = $('.t-entry-title a');
+
+    $postEntryTitles.each(function(index, titleLink) {
+      var $title = $(titleLink);
+      var titleText = $title.text();
+      var lastThreeCharacters = titleText.substring(titleText.length - 3);
+
+      if (titleText.length > 60 && lastThreeCharacters !== '...') {
+        var truncatedText = titleText.substring(0, 60)
+          .split(" ")
+          .slice(0, -1)
+          .join(" ") + "...";
+        
+        $title.text(truncatedText);
+      }
+    });
+  }
 })(jQuery)
