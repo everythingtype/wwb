8a08936b3193a79c765d3b3b816e2b0bd2a7b494
Wuelber Castillo <wuelber.castillo@gmail.com>
2019-02-21 16:42:38 -0600

    Add first level nav item on dropwdown menu for mobile

--- wp-content/themes/uncode-child/app.js
+++ wp-content/themes/uncode-child/app.js
@@ -3,13 +3,15 @@
     $('.team-members-section #board').remove();
     $('.team-members-section div[data-id="staff"]').find('.t-entry-visual').remove();
 
-    if ($(window).width() <=800 ) {
-      truncateAllBlogTitleInMobile();
-      $( document ).ajaxComplete(truncateAllBlogTitleInMobile);
+    if ($(window).width() <= 959 ) {
+      truncateAllBlogTitles();
+      $( document ).ajaxComplete(truncateAllBlogTitles);
+
+      addFirstLevelItemOnDropdownMenuNav();
     }    
   });
 
-  function truncateAllBlogTitleInMobile() {
+  function truncateAllBlogTitles() {
     var $postEntryTitles = $('.t-entry-title a');
 
     $postEntryTitles.each(function(index, titleLink) {
@@ -24,6 +26,21 @@
           .join(" ") + "...";
         
         $title.text(truncatedText);
+      }
+    });
+  }
+
+  function addFirstLevelItemOnDropdownMenuNav () {
+    var $firstLevelNavItems = $('.menu-primary-inner > li');
+    $firstLevelNavItems.each(function(index, item) {
+      var itemText = $(item).children('a.dropdown-toggle').text();
+      var itemLink = $(item).children('a.dropdown-toggle').attr('href');
+      var $dropMenu = $(item).find('.drop-menu');
+
+      if ($dropMenu.length) {
+        var liElementClasses = 'menu-item menu-item-type-custom menu-item-object-custom';
+        $dropMenu.prepend('<li class="'+ liElementClasses +'"><a title="'+ itemText +'" href="'+ itemLink +'">'+ itemText +'</a></li>');
+        $(item).children('a.dropdown-toggle').removeAttr('href');
       }
     });
   }
