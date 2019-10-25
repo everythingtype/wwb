b0c78972219820b4bb0b8044bd7d0564e83f2e3a
Pierre <pierre@etc-nyc.com>
2019-10-25 11:53:44 -0400

    test

---
+++ wp-content/wp-rocket-config/wwb.test.php
@@ -0,0 +1,18 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = 'bca0f28984688eb7946891e3c2656de0';
+$rocket_cache_mobile_files_tablet = 'desktop';
+$rocket_cache_mobile = '1';
+$rocket_do_caching_mobile_files = '1';
+$rocket_cache_ssl = '1';
+$rocket_cache_reject_uri = '/(.+/)?feed/?|/(index\.php/)?wp\-json(/.*|$)';
+$rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_reject_ua = 'facebookexternalhit';
+$rocket_cache_query_strings = array (
+  0 => 'q',
+);
+$rocket_secret_cache_key = '5c782f0ba54d9776312553';
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
